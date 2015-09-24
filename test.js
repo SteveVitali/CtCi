e6cd409f80e51cd7309228bb059a34f48922ec21
Steven Vitali <svitali@seas.upenn.edu>
2015-09-24 01:01:07 -0400

    Add handy 'test' module for testing algorithms

---
+++ test.js
@@ -0,0 +1,30 @@
+var _ = require('lodash');
+
+/**
+ * Test a function with an array of arguments on a set of cases
+ * @param  {Function} func The function to test
+ * @param  {Array} cases   An array of 'case' objects w/ args/output
+ * @return {Boolean} True if all cases pass, false otherwise
+ */
+exports.test = function(func, cases) {
+  return _.reduce(cases, function(memo, c) {
+    // NOTE: only works for primitive output types
+    var success = func.apply(this, c.input) === c.output;
+    var result = success ? 'succeeds' : 'fails';
+    console.log(func.name + ' ' + result + ' for input "' + c.input + '"');
+    return success && memo;
+  }, true);
+};
+
+/**
+ * Make a 'case' object with input and output values
+ * @param  {Array}  inputArgs Array of input args (or just one arg)
+ * @param  {Native} output    Output value (cannot be object or array)
+ * @return {Object}           The 'case' object
+ */
+exports.makeCase = function(inputArgs, output) {
+  return {
+    input: _.isArray(inputArgs) ? inputArgs : [inputArgs],
+    output: output
+  };
+};
