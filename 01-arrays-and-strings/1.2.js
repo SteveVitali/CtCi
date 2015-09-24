bbd107c9d41b97e1a34388073e51824f6f808a3d
Steven Vitali <svitali@seas.upenn.edu>
2015-09-24 01:10:52 -0400

    Add solution for 1.2

---
+++ 01-arrays-and-strings/1.2.js
@@ -0,0 +1,21 @@
+var test = require('../test');
+
+// 1.2 Implement a function void reverse(char* str)
+// in C or C++ which reverses a null-terminated string
+function reverse(str) {
+  var reverse = '';
+  var count = str.length;
+  while (--count >= 0) reverse += str[count];
+  return reverse;
+}
+
+test.test(reverse, [
+    ['', ''],
+    ['a', 'a'],
+    ['ab', 'ba'],
+    ['racecar', 'racecar'],
+    ['javascript', 'tpircsavaj']
+  ].map(function(c) {
+    return test.makeCase(c[0], c[1]);
+  })
+);
