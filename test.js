504ec676e3d23b181eb6ec3782cce16a02be6933
Steven Vitali <svitali@seas.upenn.edu>
2015-10-21 15:59:01 -0400

    Add structural-equals function and beautify console output

--- test.js
+++ test.js
@@ -28,6 +28,49 @@ var format = function(input) {
 };
 
 /**
+ * Format function arguments as tuple
+ * @param  {Any[]}  input Array of functiona rgs
+ * @return {String} The args represented as a tuple
+ */
+var formatInput = function(args) {
+  return '(' + _.reduce(args, function(memo, value, index) {
+    return format(value) + (index !== args.length - 1 ? ', ' : '');
+  }, '') + ')';
+};
+
+/**
+ * Tell whether two values are structurally equal
+ * @param  {Any} a   The first value
+ * @param  {Any} b   The second value
+ * @return {Boolean} Whether they're structurally equal
+ */
+var equals = function equals(a, b) {
+  // Handle native types and references
+  if (a === b || a == b) return true;
+  // Handle arrays
+  if (_.isArray(a)) {
+    if (!_.isArray(b) || b.length !== a.length){
+      return false;
+    }
+    return _.reduce(a, function(memo, value, index) {
+      return memo && equals(a[index], b[index]);
+    }, true);
+  }
+  // Handle objects without prototype chain
+  if (_.isObject(a)) {
+    if (!_.isObject(b)) return false;
+    var isEqual = true;
+    for (var key in a) {
+      if (a.hasOwnProperty(key)) {
+        isEqual = isEqual && b.hasOwnProperty(key) && equals(a[key], b[key]);
+      }
+    }
+    return isEqual;
+  }
+  return false;
+};
+
+/**
  * Log the result of the test to the console
  * @param  {Function} func The function being tested
  * @param  {Object}   expected The 'case' object for the test
@@ -35,9 +78,9 @@ var format = function(input) {
  */
 function logResult(func, testCase, output) {
   var expect = testCase.output;
-  var success = expect === output;
+  var success = equals(expect, output);
   var resultStr = success ? 'succeeds' : 'fails';
-  var inputStr = 'for input <' + format(testCase.input) + '>.';
+  var inputStr = 'for input <' + formatInput(testCase.input) + '>.';
   console.log(
     func.name + ' ' + resultStr + ' ' + inputStr + (
     !success
