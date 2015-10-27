653e9c4efc71520a122a9224d7d90d2a92a9deb4
Steven Vitali <svitali@seas.upenn.edu>
2015-10-26 20:41:33 -0400

    Add binary search utility to array module

--- data-structures/array.js
+++ data-structures/array.js
@@ -1,1 +1,21 @@
-module.exports = {};
+module.exports = {
+  /**
+   * Given a sorted array, find the value in O(lgn) time
+   * @param  {Array}  arr A sorted array
+   * @param  {Any}    val The value to search for in arr
+   * @param  {Number} l   The left index to start search at (inclusive)
+   * @param  {Number} r   The right index to end search at (exclusive)
+   * @return {Number} The index of the value, or undefined
+   */
+  binarySearch: function(arr, val, l, r) {
+    var search = function search(l, r) {
+      if (l === r) return arr[l] === val ? l : undefined;
+      var mid = Math.floor((r + l) / 2);
+
+      if (arr[mid] === val) return mid;
+      if (arr[mid] > val) return search(l, mid);
+      if (arr[mid] < val) return search(mid + 1, r);
+    };
+    return search(l || 0, r || arr.length);
+  }
+};
