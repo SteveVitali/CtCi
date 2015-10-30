797c5fcb901156143cf8e4b696319ebe455f24c2
Steven Vitali <svitali@seas.upenn.edu>
2015-10-29 22:52:57 -0400

    Fininsh tweaking/testing BST remove/etc.

--- data-structures/trees/bst.js
+++ data-structures/trees/bst.js
@@ -12,31 +12,35 @@ var TreeNode = function(data, parent, left, right) {
 };
 
 /**
- * Get the minimum element from a node
- * @return {Any} The smallest element
+ * Get the minimum child Node
+ * @return {Any} The child Node with largest value
  */
-TreeNode.prototype.getMinimum = function getMinimum() {
-  return (function getMinimum(node) {
-    return node.left ? getMinimum(node.left) : node.val;
-  })(this);
+TreeNode.prototype.getMinimum = function() {
+  return this.left ? this.left.getMinimum() : this;
 };
 
+/**
+ * Get the minimum value in the BST
+ * @return {Any} The value of the smallest child
+ */
 BST.prototype.getMinimum = function getMinimum() {
-  return this.root && this.root.getMinimum();
+  return this.root && this.root.getMinimum().data;
 };
 
-/**g
- * Get the maximum element from a node
- * @return {Any} The largest element
+/**
+ * Get the maximum child Node
+ * @return {Any} The child Node with largest value
  */
 TreeNode.prototype.getMaximum = function() {
-  return (function getMaximum(node) {
-    return node.right ? getMaximum(node.right) : node.val;
-  })(this);
+  return this.right ? this.right.getMaximum() : this;
 };
 
+/**
+ * Get the maximum value in the BST
+ * @return {Any} The value of the largest child
+ */
 BST.prototype.getMaximum = function() {
-  return this.root && this.root.getMaximum();
+  return this.root && this.root.getMaximum().data;
 };
 
 /**
@@ -70,7 +74,7 @@ BST.prototype.insert = function(val) {
  * @param  {TreeNode} replacement The replacement node
  * @return {Boolean}  Whether the replacement was succesful
  */
-TreeNode.replaceWith = function(replacement) {
+TreeNode.prototype.replaceWith = function(replacement) {
   if (this.parent) {
     if (this == this.parent.left) {
       this.parent.left = replacement;
@@ -78,7 +82,7 @@ TreeNode.replaceWith = function(replacement) {
       this.parent.right = replacement;
     }
   }
-  replacement.parent = this.parent;
+  replacement && (replacement.parent = this.parent);
 };
 
 /**
@@ -95,11 +99,12 @@ TreeNode.prototype.remove = function remove(val) {
     this.data = successor.data;
     return successor.remove(successor.data);
   }
-  return this.replaceWith(this.left || this.right || null);
+  this.replaceWith(this.left || this.right || null);
+  return true;
 };
 
 BST.prototype.remove = function(val) {
-  return this.root && this.root.remove(val);
+  return !!this.root && this.root.remove(val);
 };
 
 /**
@@ -131,17 +136,17 @@ var test = function() {
   var assert = require('chai').assert;
   var tree = new BST();
 
-  assert.isFalse(tree.search(1));
-  assert.isFalse(tree.searchIterative(1));
-  assert.equal(tree.getMinimum(), 1);
-  assert.equal(tree.getMaximum(), 1);
+  assert.isFalse(tree.search(4));
+  assert.isFalse(tree.searchIterative(4));
 
-  tree.insert(4);
+  assert.isTrue(tree.insert(4));
+  assert.isFalse(tree.insert(4));
+
+  assert.equal(tree.getMinimum(), 4);
+  assert.equal(tree.getMaximum(), 4);
 
   assert.isTrue(tree.search(4));
   assert.isTrue(tree.searchIterative(4));
-  assert.equal(tree.getMinimum(), 1);
-  assert.equal(tree.getMaximum(), 4);
 
   var vals = [5, 3, 1, 2, 0, 3.5, 3.2, 3.8, 4.5, 6, 7, 10];
   _.each(vals, function(v) { tree.insert(v); });
@@ -151,22 +156,26 @@ var test = function() {
   assert.equal(tree.getMaximum(), 10);
 
   // Simple delete: no children
-  tree.remove(0);
+  assert.isTrue(tree.remove(0));
 
   assert.isFalse(tree.search(0));
 
   // Delete with one child tree
-  tree.remove(6);
+  assert.isTrue(tree.remove(6));
 
   assert.isFalse(tree.search(6));
 
   // Delete the root!
-  tree.remove(4);
-  assert.equal(tree.root.value, 4.5);
+  assert.isTrue(tree.remove(4));
+  assert.equal(tree.root.data, 4.5);
   assert.isFalse(tree.search(4));
+
+  assert.isFalse(tree.remove(0));
+  assert.isFalse(tree.remove(6));
+  assert.isFalse(tree.remove(4));
 };
 
-// test();
+test();
 
 module.exports = {
   BST: BST,
