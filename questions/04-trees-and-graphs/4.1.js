b9500a723266cac1654e55c9f62a9bdea7e35878
Steven Vitali <svitali@seas.upenn.edu>
2015-11-09 02:05:44 -0500

    Add solution to 4.1

---
+++ questions/04-trees-and-graphs/4.1.js
@@ -0,0 +1,5 @@
+// Implement a function to check if a binary tree is balanced.
+// (heights of the two subtrees of a ndoe never differ by more than 1)
+/**
+ * Solved in ../../data-structures/binary-tree.js
+ */
