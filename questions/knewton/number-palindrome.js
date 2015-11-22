34bee9768b89a6eeb49fa86ca11752fc882f44a9
Steven Vitali <svitali@seas.upenn.edu>
2015-11-22 17:37:27 -0500

    Add solution to number-palindrome without casting to string

--- questions/knewton/number-palindrome.js
+++ questions/knewton/number-palindrome.js
@@ -1,11 +1,11 @@
 var test = require('../../test');
 
 /**
- * Return true if number is a palindrome
+ * Return true if number is a palindrome, using casting
  * @param  {Number} num The Number
  * @return {Boolean}    Whether number is a palindrome
  */
-function numberPalindrome(num) {
+function numberPalindromeWithCasting(num) {
   var str = num + '';
   for (var i = 0; i < Math.floor(str.length / 2); i++) {
     if (str[i] !== str[str.length - i - 1]) {
@@ -15,7 +15,27 @@ function numberPalindrome(num) {
   return true;
 }
 
-test.test(numberPalindrome, [
+/**
+ * Determine whether a number if a palindrome W/O CASTING
+ * @param  {Number} num The Number to check palindromicity of
+ * @return {Boolean}    Whether number is a palindrome
+ */
+function numberPalindromeWithoutCasting(num) {
+  var getDigit = function(i) {
+    return Math.floor(
+      (num % Math.pow(10, i + 1)) / Math.pow(10, i)
+    );
+  };
+  var size = Math.floor(Math.log(num) / Math.log(10));
+  for (var i = 0; i < size; i++) {
+    if (getDigit(i) !== getDigit(size - i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+var cases = [
   [[0], true],
   [[9], true],
   [[11], true],
@@ -29,4 +49,7 @@ test.test(numberPalindrome, [
   [[34543], true],
   [[12345], false],
   [[123321], true]
-].map(test.makeCaseFromArray));
+].map(test.makeCaseFromArray);
+
+test.test(numberPalindromeWithCasting, cases);
+test.test(numberPalindromeWithoutCasting, cases);
